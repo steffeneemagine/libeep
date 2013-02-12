7b62cad55c30c60019ae38a4a5c03c1c30cd2cac
rsmies <rsmies@5518893d-8fc4-48d3-b63f-4234d993071f>
2013-02-12 08:28:17 +0000

    fixed memory allocation error.

--- test/demo_read.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/tmp/libeep/lib:/usr/lib/jvm/java-6-openjdk-i386/jre/lib/i386/xawt
-export CLASSPATH=$(dirname $0):/tmp/libeep/lib/libeep.jar
-
-javac $(dirname $0)/demo_read.java
-java demo_read $*
