f5b24d2bbc927b49ca0bc1858fdb83240d6d029c
rsmies <rsmies@5518893d-8fc4-48d3-b63f-4234d993071f>
2013-02-14 10:30:42 +0000

    removed script.

--- test/demo_read.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# assuming libeep was installed in /tmp/libeep
-export LIBEEP_PREFIX=/tmp/libeep
-export LD_LIBRARY_PATH=${LIBEEP_PREFIX}/lib:/usr/lib/jvm/java-6-sun-1.6.0.26/jre/lib/i386/xawt
-export CLASSPATH=$(dirname $0):${LIBEEP_PREFIX}/lib/libeep.jar
-
-javac $(dirname $0)/demo_read.java
-java demo_read $*
