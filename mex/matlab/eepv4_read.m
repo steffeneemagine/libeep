9a0a53f0061652fd045d418d635f09f4fcc29235
rsmies <rsmies@5518893d-8fc4-48d3-b63f-4234d993071f>
2017-02-15 15:21:31 +0000

    fixed typos and copy-and-paste error

--- mex/matlab/eepv4_read.m
+++ mex/matlab/eepv4_read.m
@@ -2,14 +2,14 @@ function [data] = eepv4_read(fn);
 
 % eepv4_read reads data from aa cnt or avg file
 %
-% data = eepv4_read_info(filename, sample1, sample2)
+% data = eepv4_read(filename, sample1, sample2)
 %
 % where sample1 and sample2 are the begin and end sample of the data
 % to be read, starting at one sample2 being non-inclusive
 %
 % data then contains:
 %
-% info.version          ... version of the software
+% data.version          ... version of the software
 % data.samples          ... array [nchan x npnt] containing eeg data (uV)
 % data.triggers         ... array [offset_in_file, offset_in_segment, seconds_in_file, seconds_in_segment, label, duration, type, code, condition, videofilename, impedances] trigger info, where each the fields are:
 %                             offset_in_file     ... sample offset(starting at 0) of the trigger from the beginning of the file
